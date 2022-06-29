1431fbc6833f2b21124722ac79eba52bee23c4b4
JurisConcept <108427853+JurisConcept@users.noreply.github.com>
2022-06-29 16:58:46 -0400

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://jeapi-swagger.s3.ca-central-1.amazonaws.com/JEWebAPI_swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
